cde837c4d2199be9fdb02d2d43f938bcbfe8f6f8
Anna ten Bensel <89222426+atenbensel@users.noreply.github.com>
2025-10-29 01:50:21 +0000

    load data

--- notebooks/03_bayesian_blend/ep4_bayesian_blend.py
+++ notebooks/03_bayesian_blend/ep4_bayesian_blend.py
@@ -11,3 +11,10 @@ FACTOR_DIR = "results/ep2_factor_engine"
 TFT_DIR = "results/ep3_tft_alpha"
 OUTDIR = "results/ep4_bayesian_blend"
 os.makedirs(OUTDIR, exist_ok=True)
+
+prices = pd.read_csv(os.path.join(DATA_DIR, "prices_sp100.csv"), index_col=0, parse_dates=True)
+fwd1 = prices.pct_change().shift(-1)
+
+mom_curve = pd.read_csv(os.path.join(FACTOR_DIR, "MOM_6M_skip1w_curves.csv"), index_col=0)
+def_curve = pd.read_csv(os.path.join(FACTOR_DIR, "DEF_vol_1M_curves.csv"), index_col=0)
+val_curve = pd.read_csv(os.path.join(FACTOR_DIR, "VAL_proxy_curves.csv"), index_col=0)
