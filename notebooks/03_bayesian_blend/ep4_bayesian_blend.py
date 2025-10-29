f61e30b930a3ee955f5b237e17d085e6c91a9585
Anna ten Bensel <89222426+atenbensel@users.noreply.github.com>
2025-10-29 01:49:38 +0000

    config

--- notebooks/03_bayesian_blend/ep4_bayesian_blend.py
+++ notebooks/03_bayesian_blend/ep4_bayesian_blend.py
@@ -5,3 +5,9 @@ from scipy import stats
 import matplotlib.pyplot as plt
 
 pd.options.display.float_format = "{:,.6f}".format
+
+DATA_DIR = "data/raw"
+FACTOR_DIR = "results/ep2_factor_engine"
+TFT_DIR = "results/ep3_tft_alpha"
+OUTDIR = "results/ep4_bayesian_blend"
+os.makedirs(OUTDIR, exist_ok=True)
